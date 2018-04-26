68c92a3dc059fa40aba04d77b28c1a2e2112a7b6
Evgenii Tursin <tursinevgenii@gmail.com>
2018-04-26 23:31:59 +0300

    Добавил список search_history, которая сохраняет историю поиска

--- HW_2.4.py
+++ HW_2.4.py
@@ -26,19 +26,37 @@ def sql_files():
 
 def search_string(sql_files_list):
     file_list = sql_files_list
+    search_history = []
     while True:
         search = input('Введите строку, которая содержится в файлах: ')
         search = search.lower()
         founds_files = []
-        for file_name in file_list:
-            if search in decode_file(file_name):
-                founds_files.append(file_name)
-                print(file_name)
-        print('Всего: {}'.format(len(founds_files)))
-        file_list = founds_files
+
+        if search != 'go back':
+            for file_name in file_list:
+                if search in decode_file(file_name):
+                    founds_files.append(file_name)
+                    print(file_name)
+            search_history.append(founds_files)
+            print('Всего: {}'.format(len(founds_files)))
+            file_list = founds_files
+
+        if search == 'go back':
+            del search_history[-1]
+            try:
+                for i in range(len(search_history[-1])):
+                    print(search_history[-1][i], end='\n')
+                print('Всего: {}'.format(len(search_history[-1])))
+
+            except IndexError:
+                print('К сожалению, история пустая')
 
 
 if __name__ == '__main__':
+    print('*' * 140)
+    print('Введите часть содержимого файла, например "INSERT".')
+    print('Если вы хотите посмотреть результат предыдущего поиска введите "go back".')
+    print('*' * 140)
     search_string(sql_files())
 
     pass
